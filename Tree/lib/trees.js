02a3d004c20e4923698f0efb331798a997c6a4ba
Keith Dahlby <keith@newbo.co>
2018-08-31 13:27:34 -0500

    Fix up Node/Tree class definitions; adjust empty insert behavior

--- Tree/lib/trees.js
+++ Tree/lib/trees.js
@@ -1,55 +1,63 @@
 'use strict';
 
-class Tree {
-  Node(value) {
+class Node {
+  constructor(value) {
     this.value = value;
     this.right = null;
     this.left = null;
     this.parent = null;
   }
-  Root() {
-    this.root = null;
-  }
-}
-Tree.prototype.insert = function(value) {
-  if (this.root !== null) {
-    this.root === value
-  }
-  let node = new Node(value);
-  let current = this.root;
-  while(current.right !== null || current.left !== null) {
-    if(current.value > node) {
-      if(current.left === null) {
-        node.parent = current;
-        current.left = node;
-      } else {
-        current = current.left
-      }
-    } if(current.value < node) {
-      if(current.right === null) {
-        node.parent = current;
-        current.right = node;
-      } else {
-        current = current.right;
-      }
-    } else if(current.value === node) {
-      return null;
-    }
-  }
 }
 
-Tree.prototype.find = function(value) {
-  let current = this.root;
-  while(current.value !== null) {
-    if(current.value < value) {
-      current = current.right;
-    } if (current.value > value) {
-      current = current.left;
-    } if (current.value === value) {
-      return current;
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let node = new Node(value);
+
+    // Tree is empty, so value is our new root
+    if (this.root === null) {
+      this.root = node;
+      return;
     }
-  } if (current.value !== value) {
-    return null;
+
+    let current = this.root;
+    while(current.right !== null || current.left !== null) {
+      if(current.value > node) {
+        if(current.left === null) {
+          node.parent = current;
+          current.left = node;
+        } else {
+          current = current.left
+        }
+      } if(current.value < node) {
+        if(current.right === null) {
+          node.parent = current;
+          current.right = node;
+        } else {
+          current = current.right;
+        }
+      } else if(current.value === node) {
+        return null;
+      }
+    }
+  }
+
+  find(value) {
+    let current = this.root;
+    while(current.value !== null) {
+      if(current.value < value) {
+        current = current.right;
+      } if (current.value > value) {
+        current = current.left;
+      } if (current.value === value) {
+        return current;
+      }
+    } if (current.value !== value) {
+      return null;
+    }
   }
 }
 
