e6dbdd513d327dcb1a31fd3c25a731f9a4949f84
Keith Dahlby <keith@newbo.co>
2018-09-06 14:16:15 -0500

    Test cleanup to make sure we are checking actual result list

--- Tree/__test__/bft.test.js
+++ Tree/__test__/bft.test.js
@@ -3,22 +3,22 @@
 
 const bFT = require('../lib/bft');
 const Tree = require('../lib/trees');
-const Queue = require('../../stackqueue/lib/queue');
-const LL = require('../../linked-list/lib/linked-list');
 
 describe('BFT', () => {
-  let tree;
-  let queue;
-  let bft;
-  let ll;
   it('returns null for an empty tree', () => {
-    tree = new Tree();
-    expect(tree.root).toBe(null);
+    // Arrange
+    let tree = new Tree();
+
+    // Act
+    let ll = bFT(tree);
+
+    // Assert
+    expect(ll).toBe(null);
   });
+
   it('can move a tree\'s nodes from left to right starting from the root into a linked list', () => {
-    tree = new Tree();
-    queue = new Queue();
-    ll = new LL();
+    // Arrange
+    let tree = new Tree();
     tree.insert(5);
     tree.insert(10);
     tree.insert(4);
@@ -26,11 +26,19 @@ describe('BFT', () => {
     tree.insert(3);
     tree.insert(12);
     tree.insert(6);
-    bFT(tree);
-  expect(ll.head).toBe(5);
+
+    // Act
+    var ll = bFT(tree);
+
+    // Assert
+    expect(ll.head).toBe(5);
   });
+
   it('can return null for no tree input', () => {
-    bFT('hello');
-    expect(ll.head).toBe(null);
-  })
+    // Act
+    var ll = bFT('hello');
+
+    // Assert
+    expect(ll).toBe(null);
+  });
 });
